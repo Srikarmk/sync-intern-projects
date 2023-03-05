051e93d7a99c762d9c4f3000163def614f91ba53
SrikarMK <srikarmks@gmail.com>
2023-03-05 22:53:44 +0530

    Add files via upload

---
+++ Quiz App/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is Taj Mahal a symbol for?",
+    answer: "Love",
+    options: [
+      "Jealousy",
+      "Love",
+      "Hate",
+      "Anxiety"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How long is The Great Wall of China",
+    answer: "21,196 km",
+    options: [
+      "20 km",
+      "200 km",
+      "40,000 km",
+      "21,196 km"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where is Chichén Itzá located?",
+    answer: "Mexico",
+    options: [
+      "Mexico",
+      "Jordan",
+      "India",
+      "Peru"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many notable wonders are present in the world?",
+    answer: "7",
+    options: [
+      "4",
+      "12",
+      "9",
+      "7"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where is the Colosseum located?",
+    answer: "Rome",
+    options: [
+      "Rome",
+      "Agra",
+      "Spain",
+      "Sydney"
+    ]
+  }
+];
